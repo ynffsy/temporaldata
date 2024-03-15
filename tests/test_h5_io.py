5dbd2fbdd86a22b60f05dc6fab005dac862c6a62
Mehdi Azabou <36160333+mazabou@users.noreply.github.com>
2024-03-15 09:42:42 -0400

    fix unit tests (#162)
    * fix unit tests
    
    * update test workflow
    
    * rm pip downgrade from README

--- tests/test_h5_io.py
+++ tests/test_h5_io.py
@@ -22,30 +22,33 @@ def test_filepath(request):
 
 
 def test_save_to_hdf5(test_filepath):
-    a = IrregularTimeSeries(np.array([0, 1, 2]), x=np.array([1, 2, 3]))
+    a = IrregularTimeSeries(
+        timestamps=np.array([0.0, 1.0, 2.0]), x=np.array([1, 2, 3]), domain="auto"
+    )
 
     with h5py.File(test_filepath, "w") as file:
         a.to_hdf5(file)
 
-    b = Interval(start=np.array([0, 1, 2]), end=np.array([1, 2, 3]))
+    b = Interval(start=np.array([0.0, 1.0, 2.0]), end=np.array([1, 2, 3]))
 
     with h5py.File(test_filepath, "w") as file:
         b.to_hdf5(file)
 
-    c = RegularTimeSeries(x=np.random.random((100, 48)), sampling_rate=10)
+    c = RegularTimeSeries(
+        x=np.random.random((100, 48)), sampling_rate=10, domain=Interval(0.0, 10.0)
+    )
 
     with h5py.File(test_filepath, "w") as file:
         c.to_hdf5(file)
 
     d = Data(
-        start=0,
-        end=3,
         a_timeseries=a,
         b_intervals=b,
         c_timeseries=c,
         x=np.array([0, 1, 2]),
         y=np.array([1, 2, 3]),
         z=np.array([2, 3, 4]),
+        domain=Interval(0.0, 3.0),
     )
 
     with h5py.File(test_filepath, "w") as file:
@@ -54,7 +57,9 @@ def test_save_to_hdf5(test_filepath):
 
 def test_load_from_h5(test_filepath):
     # create a file and save it
-    a = IrregularTimeSeries(np.array([0, 1, 2]), x=np.array([1, 2, 3]))
+    a = IrregularTimeSeries(
+        np.array([0.0, 1.0, 2.0]), x=np.array([1.0, 2.0, 3.0]), domain="auto"
+    )
     with h5py.File(test_filepath, "w") as file:
         a.to_hdf5(file)
 
@@ -80,18 +85,21 @@ def test_load_from_h5(test_filepath):
         assert np.all(b.start[:] == np.array([0, 1, 2]))
         assert np.all(b.end[:] == np.array([1, 2, 3]))
 
-    a = IrregularTimeSeries(np.array([0, 1, 2]), x=np.array([1, 2, 3]))
+    a = IrregularTimeSeries(
+        np.array([0.0, 1.0, 2.0]), x=np.array([1.0, 2.0, 3.0]), domain="auto"
+    )
     b = Interval(start=np.array([0, 1, 2]), end=np.array([1, 2, 3]))
-    c = RegularTimeSeries(x=np.random.random((100, 48)), sampling_rate=10)
+    c = RegularTimeSeries(
+        x=np.random.random((100, 48)), sampling_rate=10, domain=Interval(0.0, 10.0)
+    )
     d = Data(
-        start=0,
-        end=3,
         a_timeseries=a,
         b_intervals=b,
         c_timeseries=c,
         x=np.array([0, 1, 2]),
         y=np.array([1, 2, 3]),
         z=np.array([2, 3, 4]),
+        domain=Interval(0.0, 3.0),
     )
 
     with h5py.File(test_filepath, "w") as file:
@@ -109,37 +117,3 @@ def test_load_from_h5(test_filepath):
         assert np.all(d.x[:] == np.array([0, 1, 2]))
         assert np.all(d.y[:] == np.array([1, 2, 3]))
         assert np.all(d.z[:] == np.array([2, 3, 4]))
-
-
-def test_lazy_slicing(test_filepath):
-    # create a file and save it
-    a = IrregularTimeSeries(
-        np.array([0.1, 0.3, 1.0, 1.2, 2.4]), x=np.array([1, 2, 3, 4, 5])
-    )
-    with h5py.File(test_filepath, "w") as file:
-        a.to_hdf5(file)
-
-    del a
-
-    # load it again
-    with h5py.File(test_filepath, "r") as file:
-        a = IrregularTimeSeries.from_hdf5(file)
-
-        assert a._lazy, "object should be lazy"
-        assert isinstance(a.timestamps, h5py.Dataset), "timestamps should be a dataset"
-
-        b = a.slice(0.2, 1.15)
-        assert not b._lazy, "object should not be lazy"
-        assert isinstance(
-            b.timestamps, np.ndarray
-        ), "timestamps should be a numpy array"
-        assert np.all(b.timestamps == np.array([0.3, 1.0]) - 0.2)
-        assert np.all(b.x == np.array([2, 3]))
-
-        c = a.slice(0.0, 0.8)
-        assert np.allclose(c.timestamps, np.array([0.1, 0.3]))
-        assert np.all(c.x == np.array([1, 2]))
-
-        d = a.slice(0.0, 3.0)
-        assert np.allclose(d.timestamps, np.array([0.1, 0.3, 1.0, 1.2, 2.4]))
-        assert np.all(d.x == np.array([1, 2, 3, 4, 5]))
